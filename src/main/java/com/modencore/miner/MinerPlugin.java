3a5272e95366dbf13d50a5a353a15ad4a4db0bdd
imperialroma10 <romochka.smokin@gmail.com>
2025-12-08 04:08:13 +0200

    init

---
+++ src/main/java/com/modencore/miner/MinerPlugin.java
@@ -0,0 +1,24 @@
+package com.modencore.miner;
+
+import com.liba.Liba;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinerPlugin extends JavaPlugin {
+
+    static MinerPlugin plugin;
+    Liba liba;
+    @Override
+    public void onEnable() {
+         liba = new Liba();   // my library  - For this to work, you must compile the liba project with the clean install command.
+
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    public static MinerPlugin getPlugin() {
+        return plugin;
+    }
+}
